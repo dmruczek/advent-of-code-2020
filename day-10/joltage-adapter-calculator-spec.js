f9fc22d973dd22adfb8a0bf57ed63ee4f48366a4
Dave <dmruczek@gmail.com>
2020-12-21 12:14:08 -0500

    progress on day 10 part 2

--- day-10/joltage-adapter-calculator-spec.js
+++ day-10/joltage-adapter-calculator-spec.js
@@ -9,7 +9,7 @@ describe("JoltageAdapterCalculator", function () {
             joltageAdapterCalculator.loadJoltageAdaptersFromFile("test-input.txt");
             const adapterArray = joltageAdapterCalculator.joltageAdapterArray;
             for (let i = 0; i < (adapterArray.length - 1); i++) {
-                expect (adapterArray[i] <= adapterArray[i+1]).toBe(true);
+                expect(adapterArray[i] <= adapterArray[i + 1]).toBe(true);
             }
         });
 
@@ -28,8 +28,48 @@ describe("JoltageAdapterCalculator", function () {
         });
 
     });
-    
 
-    
- 
+    describe("calculateNumberOfPossibleConnectionsForSegment", function () {
+
+        it("should properly find the number of possible connections for a given segment", function () {
+            const joltageAdapterCalculator = new JoltageAdapterCalculator();
+            let result = joltageAdapterCalculator.calculateNumberOfPossibleConnectionsForSegment([1, 2, 3, 4, 5, 7]);
+            expect(result).toBe(7);
+        });
+
+    });
+
+    describe("splitAdapterArrayIntoSegments", function () {
+
+        it("should properly split an array into segments which are separated by 3 joltage", function () {
+
+            const adapterArray = [0, 1, 2, 3, 4, 7, 8, 9, 10,
+                11, 14, 17, 18, 19, 20, 23, 24, 25,
+                28, 31, 32, 33, 34, 35, 38, 39, 42,
+                45, 46, 47, 48, 49, 52];
+
+            const expected = [
+                [0, 1, 2, 3, 4],
+                [7, 8, 9, 10, 11],
+                [14],
+                [17, 18, 19, 20],
+                [23, 24, 25],
+                [28],
+                [31, 32, 33, 34, 35],
+                [38, 39],
+                [42],
+                [45, 46, 47, 48, 49],
+                [52]
+            ];
+
+            const joltageAdapterCalculator = new JoltageAdapterCalculator();
+            let result = joltageAdapterCalculator.splitAdapterArrayIntoSegments(adapterArray);
+            expect(result).toEqual(expected);
+        });
+
+    });
+
+
+
+
 });
--- day-10/joltage-adapter-calculator.js
+++ day-10/joltage-adapter-calculator.js
@@ -1,3 +1,5 @@
+const { start } = require('repl');
+
 module.exports = class JoltageAdapterCalculator {
 
     constructor() {
@@ -20,17 +22,54 @@ module.exports = class JoltageAdapterCalculator {
         }
         this.joltageAdapterArray.push(0);
         this.joltageAdapterArray.sort(function (a, b) { return a - b; });
-        this.joltageAdapterArray.push(this.joltageAdapterArray[this.joltageAdapterArray.length-1] + 3);
+        this.joltageAdapterArray.push(this.joltageAdapterArray[this.joltageAdapterArray.length - 1] + 3);
     }
 
     getNumberOfJoltageDifferences(joltageDifference) {
         let count = 0;
         for (let i = 0; i < (this.joltageAdapterArray.length - 1); i++) {
-            if (this.joltageAdapterArray[i+1] - this.joltageAdapterArray[i] === joltageDifference) {
+            if (this.joltageAdapterArray[i + 1] - this.joltageAdapterArray[i] === joltageDifference) {
                 count++;
             }
         }
         return count;
+    }
+
+    /**
+     * Splits an adapter array into segments.  Each segment is 3 joltage away from the next segment.
+     * Array passed in must already be sorted.
+     * @param {*} adapterArray 
+     */
+    splitAdapterArrayIntoSegments(adapterArray) {
+        let segmentArray = [];
+        let startSegmentIndex = 0;
+        for (let i = 1; i < adapterArray.length; i++) {
+            if (i+1 >= adapterArray.length) {
+                segmentArray.push(adapterArray.slice(startSegmentIndex, i+1));
+            }
+            if (adapterArray[i + 1] - adapterArray[i] === 3) {
+                segmentArray.push(adapterArray.slice(startSegmentIndex, i+1));
+                startSegmentIndex = i+1;
+            }
+        }
+        return segmentArray;
+    }
+
+    calculateNumberOfPossibleConnectionsForSegment(adapterSegment) {
+
+        let removalOptions = [];
+        removalOptions.push(undefined);
+
+        for (let i = 1; i < (adapterSegment.length - 1); i++) {
+
+            if ((adapterSegment[i + 1] - adapterSegment[i - 1]) <= 3) {
+                removalOptions.push(adapterSegment[i]);
+            }
+            if (((i + 2) < adapterSegment.length) && (adapterSegment[i + 2] - adapterSegment[i - 1]) <= 3) {
+                removalOptions.push([adapterSegment[i], adapterSegment[i + 1]]);
+            }
+        }
+        return removalOptions.length;
     }
 
 };
